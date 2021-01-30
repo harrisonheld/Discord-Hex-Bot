942350b813311580694eb476f8a3e8ecc89e1ebb
itsmiir <itsmiir@users.noreply.github.com>
2021-01-30 13:14:23 -0600

    merge Player and Account

--- Discord Hex Bot/game/entity/Player.cs
+++ Discord Hex Bot/game/entity/Player.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 using Discord_Hex_Bot.game.render;
+using System.IO;
 
 namespace Discord_Hex_Bot.game.entity
 {
     class Player : MobileEntity
     {
-        public ulong id;
-
-        public Player(ulong _id, Board _board)
+        private readonly ulong id;
+        public ulong Id
         {
-            this.id = _id;
-            this.layer = RenderLayer.Main;
-            this.board = _board;
-            this.dirty = true;
+            get
+            {
+                return this.id;
+            }
+        }
+
+        public Player(ulong accountId)
+        {
+            id = accountId;
+
+            // check if this id is already in the accounts list
+            string[] lines = File.ReadAllLines(Settings.PLAYERS_PATH);
+
+            // for all lines, except the first (which is the header)
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] fields = line.Split(Settings.CSV_DELIMTIER);
+
+                ulong lineUserId = ulong.Parse(fields[0]);
+
+                // if user was found
+                if (lineUserId == accountId)
+                {
+
+                    break;
+                }
+            }
         }
     }
 }
